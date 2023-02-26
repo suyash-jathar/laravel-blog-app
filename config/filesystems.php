b8a297c6156bd8e5038df3ff30e1f7671dd18403
suyash-jathar <suyashjathar15@gmail.com>
2023-02-26 10:54:20 +0530

    Adding post,profiles in filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,20 @@ return [
             'throw' => false,
         ],
 
+        'profiles'=>[
+            'driver' => 'local',
+            'root' => storage_path('app/public/profiles'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+        
+        'posts'=>[
+            'driver' => 'local',
+            'root' => storage_path('app/public/posts'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
